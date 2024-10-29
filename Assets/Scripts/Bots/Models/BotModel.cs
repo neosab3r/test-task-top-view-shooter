8b837a19567bea5bf8908802b659a0b417515f74
Denis Gabbasov <gdidenis@mail.ru>
2024-10-29 17:46:10 +0500

    bot FSM

--- Assets/Scripts/Bots/Models/BotModel.cs
+++ Assets/Scripts/Bots/Models/BotModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using BeeGood.Managers;
 using BeeGood.View;
 using UnityEngine;
 
@@ -8,22 +9,46 @@ namespace BeeGood.Models
     {
         private Transform cachedTransform;
         private List<IBotManager> botManagers = new();
-
+        
         private bool isDeath;
         public BotModel(BotView view) : base(view)
         {
             cachedTransform = view.transform;
+            SetupManagers(new List<IBotManager>{
+                new IdleBotManager(this)
+            });
+
+        }
+
+        public void SetupManagers(List<IBotManager> root)
+        {
+            botManagers.Clear();
+            botManagers.AddRange(root);
         }
 
         public void UpdateBotManagers()
         {
-            foreach (var manager in botManagers)
+            if (botManagers.Count < 0)
             {
+                return;
+            }
+
+            for (int i = botManagers.Count - 1; i >= 0; i--)
+            {
+                var manager = botManagers[i];
                 if(manager.IsSetContext())
                 {
                     switch (manager.Evaluate())
                     {
-                        case BotManagerState.Failed
+                        case BotManagerState.Failed:
+                            continue;
+                        case BotManagerState.Success:
+                            return;
+                        case BotManagerState.Running:
+                            return;
+                        default:
+                            Debug.LogError($"Unexpected state for this {nameof(manager)}. Skipping..");
+                            return;
                     }
                 }
             }
---
+++ Assets/Scripts/Managers/FSM/IdleBotManager.cs
@@ -0,0 +1,38 @@
+﻿using BeeGood.Models;
+using UnityEngine;
+
+namespace BeeGood.Managers
+{
+    public class IdleBotContext : IManagerContext
+    {
+        public Transform PointToMove;
+
+        public IdleBotContext(Transform pointToMove)
+        {
+            PointToMove = pointToMove;
+        }
+    }
+    
+    public class IdleBotManager : BaseBotManager<IdleBotContext>
+    {
+        private BotModel parentBotModel;
+        
+        public IdleBotManager(BotModel botModel)
+        {
+            parentBotModel = botModel;
+        }
+        
+        public override bool IsSetContext()
+        {
+            var point = PatrolPointsManager.Instance.GetRandomPatrolPoint();
+            var context = new IdleBotContext(point);
+            SetContext(context);
+            return true;
+        }
+
+        public override BotManagerState Evaluate()
+        {
+            return base.Evaluate();
+        }
+    }
+}
